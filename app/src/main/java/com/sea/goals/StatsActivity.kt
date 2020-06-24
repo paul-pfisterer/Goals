c53a0a797501443a07f1ea5498ea1626a8854eb4
BILGE <S1910237026@students.fh-hagenberg.at>
2020-06-24 11:22:02 +0200

    statistic

--- app/src/main/java/com/sea/goals/StatsActivity.kt
+++ app/src/main/java/com/sea/goals/StatsActivity.kt
@@ -1,12 +1,22 @@
 package com.sea.goals
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.LegendEntry
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.LargeValueFormatter
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_stats.*
 import kotlinx.coroutines.CoroutineScope
@@ -17,6 +27,8 @@ import kotlinx.coroutines.launch
 class StatsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var drawer: DrawerLayout
     private lateinit var db: AppDatabase
+    private var dayNames = ArrayList<String>()
+    private var dayDone = ArrayList<Double>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +49,7 @@ class StatsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         db = AppDatabase.getDatabase(this)
         CoroutineScope(IO).launch {
             var data = ""
+
             val dailyGoals: List<Daily> = db.dailyGoals().getAll()
             dailyGoals.forEach {
                 data += "${it.name}/${it.id} --> Mo: ${it.monday}, Di: ${it.tuesday}, Mi: ${it.wednesday}, DO: ${it.thursday}," +
@@ -45,6 +58,22 @@ class StatsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
                         "priority: ${it.getPriority()} " +
                         "perseverance: ${it.getPeserverance()}" +
                         "\n\n"
+
+                // add name to array, and weekdays
+                dayNames.add(it.name)
+                dayDone.add(it.monday)
+                dayDone.add(it.tuesday)
+                dayDone.add(it.wednesday)
+                dayDone.add(it.thursday)
+                dayDone.add(it.friday)
+                dayDone.add(it.saturday)
+                dayDone.add(it.sunday)
+
+
+                CoroutineScope(Main).launch {
+                }
+
+
             }
             val weeklyGoals: List<Weekly> = db.weeklyGoals().getAll()
             weeklyGoals.forEach {
@@ -60,9 +89,28 @@ class StatsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
                         "\n"
             }
             CoroutineScope(Main).launch {
-                dataText.text = data
+              // dataText.text = data
+                dataText.text = dayDone.toString()
+                populateGraphData()
+
             }
+
+            var last = db.dailyGoals().getAllToday().lastIndex
+            var nameList = ArrayList<String>()
+
+
+            for(i in 0..last){
+                 nameList.add(i, db.dailyGoals().getAllToday().get(i).name)
+                db.dailyGoals().getAllToday().get(i).monday
+             }
+
         }
+
+
+        // count different goals
+        var goals = ArrayList<String>()
+
+
 
         resetTodayButton.setOnClickListener {
             CoroutineScope(IO).launch {
@@ -76,6 +124,11 @@ class StatsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
                 db.progress().removeAll()
             }
         }
+
+        // show statistic
+
+       //setBarChart()
+
     }
 
     @Override
@@ -97,4 +150,217 @@ class StatsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         }
         return true;
     }
+
+
+
+
+
+    // statistic
+    fun populateGraphData() {
+
+        var barChartView = findViewById<BarChart>(R.id.chartData)
+
+        val barWidth: Float
+        val barSpace: Float
+        val groupSpace: Float
+        val groupCount = 7
+
+        barWidth = 0.30f
+        barSpace = 0.06f
+        groupSpace = 0.60f
+
+        var xAxisValues = ArrayList<String>()
+        xAxisValues.add("Mon")
+        xAxisValues.add("Die")
+        xAxisValues.add("Mi")
+        xAxisValues.add("Do")
+        xAxisValues.add("Fr")
+        xAxisValues.add("Sa")
+        xAxisValues.add("So")
+
+
+        var yValueGroup1 = ArrayList<BarEntry>()
+
+
+        // draw the graph
+        var barDataSet1: BarDataSet
+
+
+
+        var value = 0.0
+
+        if(!dayDone.isEmpty()) {
+            for (i in 0..6) {
+
+
+                value = dayDone.get(i)
+
+                    when (i % 7) {
+
+                        1 -> yValueGroup1.add(BarEntry(2f, floatArrayOf(value.toFloat())))
+
+                        2 -> yValueGroup1.add(BarEntry(3f, floatArrayOf(value.toFloat())))
+
+                        3 -> yValueGroup1.add(BarEntry(4f, floatArrayOf(value.toFloat())))
+
+                        4 -> yValueGroup1.add(BarEntry(5f, floatArrayOf(value.toFloat())))
+
+                        5 -> yValueGroup1.add(BarEntry(6f, floatArrayOf(value.toFloat())))
+
+                        6 -> yValueGroup1.add(BarEntry(7f, floatArrayOf(value.toFloat())))
+
+                        0 -> yValueGroup1.add(BarEntry(1f, floatArrayOf(value.toFloat())))
+
+                    }
+
+            }
+        }
+
+
+
+
+
+
+
+        /*
+        yValueGroup1.add(BarEntry(1f, floatArrayOf(1.toFloat(), 3.toFloat())))
+
+        yValueGroup1.add(BarEntry(2f, floatArrayOf(1.0.toFloat(), 0.toFloat())))
+
+        yValueGroup1.add(BarEntry(3f, floatArrayOf(3.toFloat(), 7.toFloat())))
+
+        yValueGroup1.add(BarEntry(4f, floatArrayOf(1.toFloat(), 2.toFloat())))
+
+        yValueGroup1.add(BarEntry(5f, floatArrayOf(1.toFloat(), 8.toFloat())))
+
+        yValueGroup1.add(BarEntry(6f, floatArrayOf(1.toFloat(), 1.toFloat())))
+
+        yValueGroup1.add(BarEntry(7f, floatArrayOf(5.toFloat(), 0.toFloat())))
+
+         */
+
+
+
+
+
+
+
+
+
+
+        barDataSet1 = BarDataSet(yValueGroup1, "Test")
+        barDataSet1.setColors(Color.BLUE, Color.RED)
+       // barDataSet1.label = "2016"
+        barDataSet1.setDrawIcons(false)
+        barDataSet1.setDrawValues(false)
+
+        var barData = BarData(barDataSet1)
+        barChartView.data = barData // set the data and list of lables into chart
+
+
+
+        /*
+        var barData = BarData(barDataSet1)
+        barChartView.description.isEnabled = false
+        barChartView.description.textSize = 0f
+        barData.setValueFormatter(LargeValueFormatter())
+        barChartView.setData(barData)
+        barChartView.getBarData().setBarWidth(barWidth)
+        barChartView.getXAxis().setAxisMinimum(0f)
+        barChartView.getXAxis().setAxisMaximum(7f)
+        barChartView.groupBars(0f, groupSpace, barSpace)
+
+        // barChartView.groupBars(0f, groupSpace, barSpace)
+        //   barChartView.setFitBars(true)
+        barChartView.getData().setHighlightEnabled(false)
+        barChartView.invalidate()
+ */
+        // set bar label
+        var legend = barChartView.legend
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM)
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT)
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL)
+        legend.setDrawInside(false)
+
+        var legenedEntries = arrayListOf<LegendEntry>()
+
+        //dayNames.size
+
+
+        /*
+        var x = 0
+        var name = ""
+
+        for(x in 0..dayNames.size){
+           name = dayNames.get(x)
+            legenedEntries.add(LegendEntry(name, Legend.LegendForm.SQUARE, 8f, 8f,
+               null, Color.RED))
+
+        }
+
+         */
+
+
+        var x =dayNames.get(0)
+        var y =dayNames.get(1)
+
+        legenedEntries.add(LegendEntry(x, Legend.LegendForm.SQUARE, 8f, 8f, null, Color.RED))
+        legenedEntries.add(LegendEntry(y, Legend.LegendForm.SQUARE, 8f, 8f, null, Color.BLUE))
+
+
+        legend.setCustom(legenedEntries)
+
+        legend.setYOffset(2f)
+        legend.setXOffset(2f)
+        legend.setYEntrySpace(0f)
+        legend.setTextSize(5f)
+
+
+        // x Axis
+        val xAxis = barChartView.getXAxis()
+        xAxis.setGranularity(1f)
+        xAxis.setGranularityEnabled(true)
+        xAxis.setCenterAxisLabels(true)
+        xAxis.setDrawGridLines(false)
+        xAxis.textSize = 9f
+
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM)
+        xAxis.setValueFormatter(IndexAxisValueFormatter(xAxisValues))
+
+
+       // xAxis.setLabelCount(7)
+       // xAxis.mAxisMaximum = 7f
+       // xAxis.setCenterAxisLabels(true)
+       // xAxis.setAvoidFirstLastClipping(true)
+        //xAxis.spaceMin = 4f
+       // xAxis.spaceMax = 4f
+/*2
+        barChartView.setVisibleXRangeMaximum(7f)
+        barChartView.setVisibleXRangeMinimum(7f)
+        barChartView.setDragEnabled(true)
+
+        //Y-axis
+        barChartView.getAxisRight().setEnabled(false)
+        barChartView.setScaleEnabled(true)
+
+        val leftAxis = barChartView.getAxisLeft()
+        leftAxis.setValueFormatter(LargeValueFormatter())
+        leftAxis.setDrawGridLines(false)
+        leftAxis.setSpaceTop(1f)
+        leftAxis.setAxisMinimum(0f)
+
+
+        barChartView.data = barData
+        barChartView.setVisibleXRange(1f, 7f)
+
+         */
+
+
+
+
+    }
+
+
+
 }
